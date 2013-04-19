ad18ba01764f0275c47ba97e9e430ac4c43ed35f
FurtherLee <95588lsj@gmail.com>
2013-04-19 16:40:58 +0800

    first commit

---
+++ src/fatworm/sister/Launcher.java
@@ -0,0 +1,9 @@
+package fatworm.sister;
+
+public class Launcher {
+
+	
+	public static void main() {
+		// TODO
+	}
+}
---
+++ src/fatworm/sister/cmd/Cmd.java
@@ -0,0 +1,16 @@
+package fatworm.sister.cmd;
+
+public abstract class Cmd {
+	
+	public static boolean isCommand(String str) {
+		return str.startsWith("@");
+	}
+	
+	public static String stripCmdName(String str){
+		return str.substring(1);
+	}
+	
+	abstract public String getName();
+	
+	abstract public void execute();
+}
