e6badb94d21a468de9f7fda7dc940cca0797c441
FurtherLee <95588lsj@gmail.com>
2013-04-19 21:43:05 +0800

    finish fatworm sqlclient

--- src/fatworm/sister/Launcher.java
+++ src/fatworm/sister/Launcher.java
@@ -1,9 +1,82 @@
 package fatworm.sister;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import fatworm.sister.sqlcli.FatwormSqlClient;
+import fatworm.sister.sqlcli.SqlClient;
+
 public class Launcher {
 
+	private static String testName;
 	
-	public static void main() {
+	private static String url = "jdbc:fatworm://your.db";
+	
+	private static String outputFile = null;
+	
+	public static void main(String[] args) {
+		init(args);
+		launch();
+	}
+	
+	private static void init(String[] args) {
+		GnuParser parser = new GnuParser();
+		try {
+			CommandLine cl = parser.parse(getOptions(), args);
+			parseArguments(cl);
+		} catch (ParseException e) {
+			printUsage();
+		}
+	}
+	
+	private static void parseArguments(CommandLine cl) {
+		if(cl.hasOption("h")) {
+			printUsage();
+			return;
+		}
+		
+		if(!cl.hasOption("n")) {
+			System.out.println("Please Specify The Name of Test Case");
+			printUsage();
+			return;
+		}
+		
+		testName = cl.getOptionValue("n");
+		
+		if (cl.hasOption("o")) 
+			outputFile = cl.getOptionValue("o");	
+	}
+	
+	private static Options getOptions() {
+		Options ops = new Options();
+		
+		ops.addOption("n", "name", true, "The name of test suit");
+		
+		ops.addOption("o", "output", true, "The path of file to dump all output");
+		
+		ops.addOption("h", "help", false, "Print Usage");
+		
+		return ops;
+	}
+	
+	private static void printUsage() {
+		HelpFormatter hf = new HelpFormatter();
+		hf.printHelp("Fatworm Your Sister", getOptions());
+	}
+	
+	private static void launch() {
+		SqlClient client = new FatwormSqlClient();
+		client.connect(url);
+		Sister sis = new Sister(testName, client);
+		if (outputFile != null)
+			client.setOutput(outputFile);
+		sis.start();		
+	}
+	
+	private static void registerCommands(Sister sis) {
 		// TODO
 	}
 }
--- src/fatworm/sister/sqlcli/SqlClient.java
+++ src/fatworm/sister/sqlcli/SqlClient.java
@@ -1,10 +1,10 @@
 package fatworm.sister.sqlcli;
 
-import java.util.Properties;
-
 public interface SqlClient {
 
 	public void appendLine(String line);
 	
-	public void connect(String url, Properties info);
+	public void connect(String url);
+
+	public void setOutput(String outputFile);
 }
